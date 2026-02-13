d1092cda7065750d22e1a59e3f19a14d3aaa41de
Johnathan Leppert <johnathan.leppert@gmail.com>
2026-02-12 22:13:13 -0500

    add working model checkpoint

--- start_training.sh
+++ start_training.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MUJOCO_GL=egl uv run python examples/jaka_zu5_sim/train_rl.py --args.total-timesteps 100000 --args.n-envs 4 --args.eval-freq 2500 --args.checkpoint-freq 10000 --args.video-eval-freq 50000 --args.out-dir data/jaka_zu5_sim/rl_test3
+MUJOCO_GL=egl uv run python examples/jaka_zu5_sim/train_rl.py --args.total-timesteps 100000 --args.n-envs 4 --args.eval-freq 2500 --args.checkpoint-freq 10000 --args.video-eval-freq 50000 --args.out-dir data/jaka_zu5_sim/rl_test4
 
 #MUJOCO_GL=egl uv run python examples/jaka_zu5_sim/train_rl.py --args.total-timesteps 10000 --args.n-envs 4 --args.eval-freq 2500 --args.checkpoint-freq 5000 --args.video-eval-freq 100000 --args.out-dir data/jaka_zu5_sim/rl_test
 
